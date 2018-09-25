8b97ecec3ad3c15de2fe2ac74b28972ec593bdfb
Dwight Spencer <dspencer@computekindustries.com>
2018-09-25 17:37:47 -0500

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -42,6 +42,8 @@ use Cake\Routing\Router;
  */
 Router::defaultRouteClass('DashedRoute');
 
+Router::extensions(['json', 'xml']);
+
 Router::scope('/', function (RouteBuilder $routes) {
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
